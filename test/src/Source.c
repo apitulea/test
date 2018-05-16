a701f9d2b6ac8d65f9bc53a68b95996bae725563
Alexandru Cristian Pitulea <pitulea.alexandrucristian@nxp.com>
2018-05-16 12:18:27 +0300

    test uncrust

---
+++ test/src/Source.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+#define size 10
+
+
+typedef struct Stack {
+	int stack[size];
+	int top;
+
+
+}stack;
+stack* new_stack() {
+	stack* s = malloc(sizeof(stack));
+	s->top = -1;
+	return s;
+}
+
+static void push(stack *s, int value) {
+	++(s->top);
+	s->stack[s->top] = value;
+}
+
+void print(stack *s) {
+	for (int i = 0; i < s->top + 1; ++i) {
+		int value = s->stack[i];
+		printf("%i", value);
+	}
+}
+
+static void pop(stack *s) {
+	--(s->top);
+}
+
+
+int main() {
+	stack *s = new_stack();
+	push(s, 5);
+	push(s, 7);
+	push(s, 4);
+	push(s, 1);
+	print(s);
+	printf(" ");
+	pop(s);
+	print(s);
+	return 0;
+	
+}

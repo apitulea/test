864187225029b31ca378f867910c5ddb22f22126
Alexandru Cristian Pitulea <pitulea.alexandrucristian@nxp.com>
2018-05-18 17:27:34 +0300

    hopefullyyyy

---
+++ test/Source1.c
@@ -0,0 +1,26 @@
+int main()
+{
+   int  size = 0;
+   int  size1 = 0;
+   int  b = 70, a;
+   char c, d, e = 'a';
+
+   c = d = e;
+   double number = 9;
+   if ((c == d) || (size1 == b))
+   {
+   }
+   if ((c == d) || (size1 == b))
+   {
+   }
+   if (c != d)
+   {
+   }
+   if (c || d)
+   {
+   }
+   if (c)
+   {
+   }
+   return 0;
+}
